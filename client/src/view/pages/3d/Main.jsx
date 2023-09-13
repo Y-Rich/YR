4397ba32f9b0972478bcab9adc4d27a5ed02f4da
mktmf1226 <mktmf1226@gmail.com>
2023-09-13 15:19:25 +0900

    fix:Pannel에뜨는 오류 수정

--- client/src/view/pages/3d/Main.jsx
+++ client/src/view/pages/3d/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PLC from './Edukit';
 import Selector from '../../components/Selector';
 import Pannel from '../pannel/Pannel';
@@ -9,12 +9,48 @@ const Main = () => {
   const handleSelect = (option) => {
     setSelected(option);
   };
+  const [webSocket, setWebSocket] = useState(null);
+  const [messagePayloadEdukit1, setMessagePayloadEdukit1] = useState(null);
+  const [messagePayloadEnvironment, setMessagePayloadEnvironment] =
+    useState(null);
+
+  // 웹소켓 설정
+  useEffect(() => {
+    const ws = new WebSocket('ws://192.168.0.71:8080');
+
+    setWebSocket(ws);
+
+    ws.addEventListener('message', function (event) {
+      const receivedMessage = JSON.parse(event.data);
+      // console.log(receivedMessage);
+      // setMessagePayloadEdukit1(receivedMessage);
+      if (receivedMessage.topic === 'edukit1') {
+        setMessagePayloadEdukit1(JSON.parse(receivedMessage.data));
+        console.log(JSON.parse(receivedMessage.data));
+      }
+      // 환경 데이터
+      if (receivedMessage.topic === 'environment/data') {
+        setMessagePayloadEnvironment(JSON.parse(receivedMessage.data));
+        console.log(JSON.parse(receivedMessage.data));
+      }
+    });
+
+    return () => {
+      ws.close(); // 웹소켓 연결 종료
+    };
+  }, []);
+
+  const props = {
+    webSocket,
+    messagePayloadEdukit1,
+    messagePayloadEnvironment,
+  };
 
   return (
     <div>
-      {selected === 'plc' && <PLC />}
+      {selected === 'plc' && <PLC props={props} />}
       {selected === 'chart' && <Chart />}
-      {selected === 'pannel' && <Pannel />}
+      {selected === 'pannel' && <Pannel props={props} />}
       <Selector onPageChange={handleSelect} />
     </div>
   );
