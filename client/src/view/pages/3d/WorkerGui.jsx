bc2bdf8d2f017bdac1f0fa0d9e1638683dcf23cc
mktmf1226 <mktmf1226@gmail.com>
2023-09-19 22:37:55 +0900

    fix:관리자로그 수정중

--- client/src/view/pages/3d/WorkerGui.jsx
+++ client/src/view/pages/3d/WorkerGui.jsx
@@ -174,8 +174,8 @@ const WorkerGui = (props) => {
           <ToggleBtn onClick={toggleMenu}>
             <AiFillCaretRight size="20" alt="햄버거 버튼" />
           </ToggleBtn>
-          {facNum === 1 && '제 1 공장 - 상세 제어'}
-          {facNum === 2 && '제 2 공장 - 상세 제어'}
+          {facNum === 1 && '세종 공장 - 상세 제어'}
+          {facNum === 2 && '화성 공장 - 상세 제어'}
         </Box>
         {isMenuOpen && (
           <Box className="gui">
